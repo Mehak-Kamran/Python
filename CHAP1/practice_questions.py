902c6381b55dbcdca8ee2ff7537537ff9c280218
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-26 18:34:39 +0000

    Area

---
+++ CHAP1/practice_questions.py
@@ -0,0 +1,14 @@
+#ADD TWO NOS THROUGH INPUT
+
+num1=int(input("Enter no "))
+num2=int(input("Enter no "))
+print("Addition of",num1 ,"and",num2,"is",num1+num2)
+
+#AREA OF SQUARE
+
+side1=float(input("Enter First side of square"))
+side2=float(input("Enter Second side of square"))
+Area=side1*side2
+
+print("Area of given square is ",Area)
+
