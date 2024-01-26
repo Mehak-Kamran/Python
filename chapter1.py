ed04c9fc1150b0e9de7dd5c3cce95c7f9db5b6bc
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-26 18:18:04 +0000

    1

---
+++ chapter1.py
@@ -0,0 +1,26 @@
+#print
+print("Hello World")
+
+#Type Conversion are those which python automatical convert
+val1=3
+val2=4.7
+sum=val1+val2
+print('Sum is ',sum)
+#here python automatically converts val1 into float and gives output in float
+
+#Type Casting is when we force a value to change its type
+num1=1.5
+num2=4
+sum=int(num1)+num2
+print("Sum is" ,sum)
+
+#type
+print(type(sum))
+
+#
+
+
+
+
+
+
