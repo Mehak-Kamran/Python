5c23d63c0dfe720d2609c1521835066c702559b1
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-26 18:46:04 +0000

    PRACTICE QUESTIONS

--- CHAP1/practice_questions.py
+++ CHAP1/practice_questions.py
@@ -12,3 +12,18 @@ Area=side1*side2
 
 print("Area of given square is ",Area)
 
+#AVERAGE OF TWO FLOATING POINT NOS
+
+no1=float(input("Enter a no "))
+no2=float(input("Enter a no" ))
+avg=(no1+no2)/2
+
+print("Average is",avg)
+
+#IF A NO IS GREATER OR SMALL
+
+val1=int(input("Enter a no "))
+val2=int(input("Enter a no "))
+TF=val1>=val2
+
+print(TF)
