81a1ea3c8f80f2c150d06f81f5472a92a4d902d8
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-29 18:45:41 +0000

    chapter3 practice questions

---
+++ CHAP3/practice.py
@@ -0,0 +1,57 @@
+                      #LIST
+# #1                       
+# print("Enter your fav movie name , any 3")
+# movie1=input()
+# movie2=input()
+# movie3=input()
+
+# movies=[]
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
+# print(movies)
+
+# #ALTERNATIVE METHOD
+
+# movies=[]
+# print("Enter 3 movie name")
+# movies.append(input())
+# movies.append(input())
+# movies.append(input())
+# print(movies)
+
+# #2
+# list=[]
+# list.append(input("Enter anything to check if its palindrone or not"))
+# list.append(input("Enter anything to check if its palindrone or not"))
+# list.append(input("Enter anything to check if its palindrone or not"))
+# duplicate=list.copy()
+
+# duplicate.reverse()
+
+
+# if(list==duplicate):
+#     print("Palindrone")
+# else:
+#     print("Not a palindrone")
+
+
+                       #TUPLES
+#3
+tuple=("c","d","a","a","b","b","a")
+counting=tuple.count("a")
+print(counting)
+
+#4
+list=["c","d","a","a","b","b","a"]
+list.sort(reverse=True)
+#list.sort()
+
+print(list)
+
+
+
+
+
+
+
