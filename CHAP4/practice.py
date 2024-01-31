73f90da4053a7acf28588a9c9885e77ae01292bf
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-31 19:25:06 +0000

    practice question

---
+++ CHAP4/practice.py
@@ -0,0 +1,38 @@
+#1
+
+dict={
+    "cat":"a small animal",
+    "table": ["a piece of furniture","facts and figure"]
+}
+print(dict)
+
+#2
+set={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(len(set))
+
+#3
+marks={}
+mark1=int(input("Enter Automata marks "))
+marks.update({"automata":mark1})
+
+marks2=int(input("Enter Database marks "))
+marks.update({"database":marks2})
+
+marks1:int(input("Enter Dcn marks "))
+marks.update({"DCN":mark1})
+
+print(marks)
+
+#4
+no={9,'9.0'}
+print(no)
+
+#or
+value={
+    ("float","9.0"),
+    ("int","9")
+    
+
+}
+
+print(value)
