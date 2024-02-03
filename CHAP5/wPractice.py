0b05239f04a1f31702d43269d8bf6249a63a6371
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-03 07:03:31 +0000

    whileloop

---
+++ CHAP5/wPractice.py
@@ -0,0 +1,27 @@
+#1
+i=1
+while i<=100:
+    print(i)
+    i+=1
+#2
+j=100
+while j>=1:
+    print(j)
+    j-=1
+
+#3
+integer=int(input("Enter a no "))
+k=1
+while k<=10:
+    print(integer,"x",k,"=",integer*k)
+    k+=1
+
+#4
+score=[1,4,9,16,25,46,49,64,81,100]
+g=0
+while g<len(score):
+    print(score[g])
+    g+=1
+
+
+    
