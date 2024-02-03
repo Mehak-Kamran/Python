54683cbe41183c976268769da5011109c4c501a4
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-03 10:55:59 +0000

    sum and factorial

---
+++ CHAP5/Practice.py
@@ -0,0 +1,16 @@
+#1
+#input_number=int(input("Enter a number"))
+i=1
+sum=0
+#while i<=input_number:
+    #sum=sum+i
+    #i+=1
+#print(sum)
+
+#2
+fact=1
+user_inpuit=int(input("Enter a Value "))
+for no in range(user_inpuit,0,-1):
+    fact=fact*no
+print(fact)
+    
