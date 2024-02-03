042bdba40e9b5ed9e9ea26a256b357fa99d39935
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-03 09:57:05 +0000

    pass

--- CHAP5/frPractice.py
+++ CHAP5/frPractice.py
@@ -10,3 +10,13 @@
 table=int(input("Enter a no "))
 for no in range(1,11,1):
     print(table,"x",no,"=",table*no)
+
+#pass statement 
+for i in range(2):
+    pass
+
+#pass can also be used in if else 
+num=1
+if num>=2:
+    pass
+#pass is used in try or exceptional handling in python
