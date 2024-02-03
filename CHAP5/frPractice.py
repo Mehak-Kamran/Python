a13f93c43c855d7d847460df358da717dbae3f20
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-03 09:52:01 +0000

    for range

---
+++ CHAP5/frPractice.py
@@ -0,0 +1,12 @@
+#1
+#for num in range(1,101,1):
+    #print(num)
+
+#2
+#for val in range(100,0,-1):
+    #print(val)
+
+#3
+table=int(input("Enter a no "))
+for no in range(1,11,1):
+    print(table,"x",no,"=",table*no)
