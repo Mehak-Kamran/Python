349a12d5003a058e0961fc934b2f020b0f9bea8f
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-05 10:59:35 +0000

    usd to pkr

--- CHAP6/Practice.py
+++ CHAP6/Practice.py
@@ -19,14 +19,24 @@ def print_vegies(list):
 print()
 
 #3
-user_input=int(input("Enter an int "))
+#user_input=int(input("Enter an int "))
 
-def factorial(user_input):
-     fact=1
+# def factorial(user_input):
+#      fact=1
      
-     for i in range(1,user_input+1):
-        fact=fact*i
-     return fact   
+#      for i in range(1,user_input+1):
+#         fact=fact*i
+#      return fact   
         
-cal_fact= factorial(user_input)
-print(cal_fact)
+# cal_fact= factorial(user_input)
+#print(cal_fact)
+
+#4
+input_by_user=int(input("Enter usd to exchange in pkr"))
+
+def exchange(input_by_user):
+    pkr= input_by_user*279.55 
+    return pkr
+
+output=exchange(input_by_user)
+print(output)
