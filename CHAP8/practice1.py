dcefab5005330c158b41f197d7938a10b3ebde2f
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-14 18:30:34 +0000

    private class

--- CHAP8/practice1.py
+++ CHAP8/practice1.py
@@ -1,10 +1,23 @@
 #delete an object
 
-class tree:
-    def __init__(self,leaves,branch):
-        self.leaves=leaves
-        self.branch=branch
+# class tree:
+#     def __init__(self,leaves,branch):
+#         self.leaves=leaves
+#         self.branch=branch
 
-t1=tree("palmtreeleaves","brown")
-del t1
-print(t1.leaves,t1.branch)
+# t1=tree("palmtreeleaves","brown")
+# del t1
+# print(t1.leaves,t1.branch)
+
+#private class
+
+class Account:
+    def __init__(self,accountno,accountpass):
+        self.accountno=accountno
+        self.__accountpass=accountpass
+
+acc=Account("45678","abc87")
+print(acc.accountpass)
+
+#u can also make private function and private variable 
+
