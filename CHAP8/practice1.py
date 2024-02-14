d2a503eca73287a1172e508e6a8dff487ec63bbf
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-14 18:47:25 +0000

    inheritance

--- CHAP8/practice1.py
+++ CHAP8/practice1.py
@@ -11,13 +11,32 @@
 
 #private class
 
-class Account:
-    def __init__(self,accountno,accountpass):
-        self.accountno=accountno
-        self.__accountpass=accountpass
+# class Account:
+#     def __init__(self,accountno,accountpass):
+#         self.accountno=accountno
+#         self.__accountpass=accountpass
 
-acc=Account("45678","abc87")
-print(acc.accountpass)
+# acc=Account("45678","abc87")
+# print(acc.accountpass)
 
 #u can also make private function and private variable 
 
+#inheritance
+
+class laptop:
+    @staticmethod
+    def start():
+        print("laptop is started")
+    
+    @staticmethod
+    def shutdown():
+        print("laptop is shutdown")
+
+
+class HP(laptop):
+    def __init__(self,model):
+        self.model=model
+
+    
+l1=HP("Probook")
+l1.start()
