0a24c1ca131dc1d798ccc9265877f6b5c75f5bf7
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-15 16:46:04 +0000

    property

--- CHAP8/practice3.py
+++ CHAP8/practice3.py
@@ -10,3 +10,23 @@ p1.changename("eddi")
 print(p1.name)
 print(person.name)
 
+#property
+class result:
+    def __init__(self,cg,os,cc):
+        self.cg=cg
+        self.os=os
+        self.cc=cc
+        #self.per=str((cg+os+cc)/3)+"%"
+    @property
+    def percentage(self):
+        return str((self.cg+self.os+self.cc)/3)+"%"
+
+
+p1=result(71,71,88)
+print(p1.percentage)
+p1.os=91
+print(p1.percentage)
+
+
+    
+
